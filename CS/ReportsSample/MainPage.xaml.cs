0ce57b7cf9deb4ee56249ac6823c9f1dd4ac3d41
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-02 14:15:13 +0400

    Change example status to out of maintenance

--- CS/ReportsSample/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Controls;
-
-namespace ReportsSample {
-    public partial class MainPage : UserControl {
-        public MainPage() {
-            InitializeComponent();
-        }
-    }
-}
